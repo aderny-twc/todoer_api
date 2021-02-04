401e62a410e18fc81de6c00210c07abced57ff2b
Andrey Belskiy <diksandab@gmail.com>
2021-02-04 08:49:03 +0600

    Added a function that modifies data to create list of dict

--- app.py
+++ app.py
@@ -5,12 +5,33 @@ from mysqlcm import UseDataBase
 
 app = Flask(__name__)
 
+#Database configuration
 app.config['dbconfig'] = {'host': 'localhost',
                             'user': 'todoerAPI',
                             'password': 'todoerAPIpasswd',
                             'database': 'todoerAPIdb',}
 
+app.config['data_list'] = ['task_id',
+                            'title',
+                            'description',
+                            'done',
+                            'created_at']
 
+
+def db_data_modifier(key_list, contents) -> list:
+    """
+    Creates a list of dictionaries with keys passed as an argument.
+    """
+    if len(contents[0]) == len(key_list):
+        data_list = []
+        for row_data in contents:
+            data_list.append(dict(zip(key_list, row_data)))
+        return data_list
+    else:
+        return None
+
+
+#Getting all tasks for user
 @app.route('/todoer/api/tasks', methods=['GET'])
 def get_tasks():
     with UseDataBase(app.config['dbconfig']) as cursor:
@@ -21,13 +42,15 @@ def get_tasks():
                 """
         cursor.execute(_SQL)
         contents = cursor.fetchall()
-    key_list = ['task_id', 'title', 'description', 'done', 'created_at']
-    tasks_data = []
-    for row_data in contents:
-        tasks_data.append(dict(zip(key_list, row_data)))
+    tasks_data = db_data_modifier(app.config['data_list'], contents)
 
-    return jsonify(tasks_data)
+    return jsonify({'taskgs': tasks_data})
 
+
+@app.route('/todo/api/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    pass
+    
 
 
 if __name__ == '__main__':

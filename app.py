0b8c2383483b97372f1be8b048e9aec6a3a57b9e
Andrey Belskiy <diksandab@gmail.com>
2021-02-04 07:16:47 +0600

    Added database context manager; Added database config

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+app.config['dbconfig'] = {'host': 'loclalhost',
+                            'user': 'todoerAPI',
+                            'password': 'todoerAPIpasswd',
+                            'database': 'todoerAPIdb',}
+
+
 @app.route('/todoer/api/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'task': 1})

9bc616b50a41463edc334fffe914a54898fb9b28
Andrey Belskiy <diksandab@gmail.com>
2021-02-04 08:24:11 +0600

    Added simple interface for getting data by user

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
 
+from mysqlcm import UseDataBase
+
 
 app = Flask(__name__)
 
-app.config['dbconfig'] = {'host': 'loclalhost',
+app.config['dbconfig'] = {'host': 'localhost',
                             'user': 'todoerAPI',
                             'password': 'todoerAPIpasswd',
                             'database': 'todoerAPIdb',}
@@ -11,8 +13,20 @@ app.config['dbconfig'] = {'host': 'loclalhost',
 
 @app.route('/todoer/api/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'task': 1})
+    with UseDataBase(app.config['dbconfig']) as cursor:
+        _SQL = """
+                SELECT task_id, title, description, done, created_at
+                FROM tasks
+                WHERE user_id = 1
+                """
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+    key_list = ['task_id', 'title', 'description', 'done', 'created_at']
+    tasks_data = []
+    for row_data in contents:
+        tasks_data.append(dict(zip(key_list, row_data)))
 
+    return jsonify(tasks_data)
 
 
 
